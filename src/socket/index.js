f69fef63e13ce73c9097d6b22e82bc9d29e052ec
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2025-05-12 16:50:31 -0400

    Socket update

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,4 +1,4 @@
 // client/src/socket/index.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+export const socket = io("https://tcgserver.onrender.com");
